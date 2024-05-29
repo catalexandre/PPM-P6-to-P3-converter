33cbc04009afcf6d9b57c2c957a2d54a36dbb4a5
Alexandre Catellier <catellier.alexandre@gmail.com>
2024-05-29 18:13:26 -0400

    Add project files.

---
+++ PPM P6 to P3 converter.cpp
@@ -0,0 +1,47 @@
+
+#include <iostream>
+#include <fstream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    string inputFile;
+
+    cout << "name of the input file (exclude file extension): ";
+    cin >> inputFile;
+
+    ifstream image(inputFile + ".ppm", ios::binary);
+
+    if (!image.is_open())
+    {
+        cout << "Encountered a problem when opening the file";
+        return 1;
+    }
+        
+    ofstream newImage(inputFile + "P3.ppm");
+
+    newImage << "P3" << endl;
+
+    int width, height, maxValue;
+    image >> width >> height >> maxValue;
+    newImage << width << ' ' << height << endl << maxValue << endl;
+
+    image.ignore(256, '\n');
+
+    for (int y = 0; y < height; y++)
+    {
+        for (int x = 0; x < width; x++)
+        {
+            unsigned char r, g, b;
+            image.read(reinterpret_cast<char*>(&r), 1);
+            image.read(reinterpret_cast<char*>(&g), 1);
+            image.read(reinterpret_cast<char*>(&b), 1);
+            newImage << static_cast<int>(r) << " " << static_cast<int>(g) << " " << static_cast<int>(b) << " ";
+        }
+    }
+
+    image.close();
+    newImage.close();
+}
